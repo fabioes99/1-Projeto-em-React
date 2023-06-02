1d62841ba37b561b56a3333f538e9fc9d049b8ba
fabioes99 <fabioes.99@outlook.com>
2023-06-02 19:03:18 -0300

    1 commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,72 @@
+import Header from "../../components/Header";
+import background from "../../assets/GitFind.png"
+import ItemList  from "../../components/ItemList"
+import { useState } from "react";
+import './styles.css'
+
+function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setcurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async function(){
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+
+    if(newUser.name){
+      const {avatar_url, name , bio, login} = newUser;
+      setcurrentUser({avatar_url, name , bio, login});
+
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepo = await reposData.json();
+
+      if(newRepo.length){
+        setRepos(newRepo);
+      }
+      console.log(newRepo);
+    }
+  }
+
+  return (
+    <div className="App">
+     <Header />
+     <div className="conteudo">
+      <img src={background} className="background" alt=" background app" />
+      <div className="info">
+       <div>
+        <input name="usuario" value={user} onChange={ event => setUser(event.target.value) }  placeholder="@username" />
+        <button onClick={handleGetData}>Buscar</button>
+       </div>
+       {currentUser?.name ? (
+        <>
+        <div className="perfil">
+        <img src={currentUser.avatar_url} className="profile" alt="profile" />
+          <div>
+            <h3>{currentUser.name}</h3>
+            <span>@{currentUser.login}</span>
+            <p>{currentUser.bio}</p>
+          </div>
+       </div>
+        <hr />
+
+        <div>
+        <h4 className="repositorio">Repositorios</h4>
+        {repos?.length ? (
+           repos.map( repo => (
+            <ItemList key={repo.id} title={repo.name} description={repo.description} />
+          ) ) 
+        ) : null }
+         </div>
+        </>
+        
+       ) : null}
+       
+      </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
